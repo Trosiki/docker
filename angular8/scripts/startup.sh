8d28ddb8a24af70bbc356574fcb6d46e91ad84fe
Trosiki <sultanyrafa@gmail.com>
2021-05-29 21:46:11 +0200

    Instalamos Git y Añadimos config necesaria para instalar proyectos

--- angular8/scripts/startup.sh
+++ angular8/scripts/startup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 echo "END";
 
+# Its Necesary if you want use this NODE for create the project
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
 touch test
 tail -0f test
