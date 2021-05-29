818abf02d4ef80d659bb9d6f62bb120ae30e5361
Trosiki <sultanyrafa@gmail.com>
2021-05-29 22:11:17 +0200

    Working Dockers

--- angular8/scripts/startup.sh
+++ angular8/scripts/startup.sh
@@ -5,5 +5,9 @@ echo "END";
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
+### ------------------------------------------------------------------------------###
+## If "ng serve" command is not working use "ng serve --port 4200 --host 0.0.0.0" ##
+### ------------------------------------------------------------------------------###
+
 touch test
 tail -0f test
